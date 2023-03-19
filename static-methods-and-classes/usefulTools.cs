f188e35e34e573c34178356346ff48b8f3cc7c54
Matthew Chau <matthewwchau@gmail.com>
2023-03-19 20:06:52 +0000

    complete c-sharp static-methods-and-classes.

---
+++ static-methods-and-classes/usefulTools.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class UsefulTools
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        public static void SayBye(string name)
+        {
+            Console.WriteLine("Bye " + name);
+        }
+    }
+}
