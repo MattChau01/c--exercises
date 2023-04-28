9c8d8af0eb894b2f3339513a6ebbade94f903e1f
Matthew Chau <matthewwchau@gmail.com>
2023-04-28 12:55:43 -0700

    Add files via upload

---
+++ applications/cs-calculator/cs-calculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_calculator
+{
+    internal class Calculator
+    {
+        public static double Calculate(double num1, double num2, string op)
+        {
+            double result = double.NaN;
+
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                case "%":
+                    result = num1 % num2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+    }
+}
